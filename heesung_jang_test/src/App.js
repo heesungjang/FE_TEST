4dcbeb86d452bcaf4766bc8f551e36b7e088762b
Heesung jang <heesungj7@gmail.com>
2021-09-11 18:03:19 +0900

    [feat] 헤더 컴포넌트 생성
    로고 + 회사명 헤더 컴포넌트

--- heesung_jang_test/src/App.js
+++ heesung_jang_test/src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "./Components/Header"; // 헤더 컴포넌트
+
+import styled from "styled-components"; // 스타일 컴포넌트 라이브러리
 
 function App() {
-    return <div>Hello, World</div>;
+    return (
+        <MainContainer>
+            <Header />
+        </MainContainer>
+    );
 }
+
+const MainContainer = styled.div``;
 
 export default App;
