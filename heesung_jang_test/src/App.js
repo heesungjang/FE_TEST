02cf1356a6aa0df8f28b4aee0e3879bfd54eb2af
Heesung jang <heesungj7@gmail.com>
2021-09-12 01:58:22 +0900

    [refactor] 헤더 컴포넌트 삭제
    index.js로 헤더 컴포넌트 렌더링 위치 변경

--- heesung_jang_test/src/App.js
+++ heesung_jang_test/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Header from "./Components/Header"; // 헤더 컴포넌트
 
+import Result from "./page/Result"; // Result 페이지
 import styled from "styled-components"; // 스타일 컴포넌트 라이브러리
 
 function App() {
     return (
         <MainContainer>
-            <Header />
+            <Result />
         </MainContainer>
     );
 }
--- heesung_jang_test/src/index.js
+++ heesung_jang_test/src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+
+// 헤더 컴포넌트
+import Header from "./Components/Header";
+// 스타일 컴포넌트 themeProvider
 import GlobalThemeProvider from "./styles/GlobalThemeProvider";
 
 ReactDOM.render(
     <React.StrictMode>
         <GlobalThemeProvider>
+            <Header />
             <App />
         </GlobalThemeProvider>
     </React.StrictMode>,
