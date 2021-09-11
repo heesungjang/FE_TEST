80523d2241016a833996615367f4360a46176f2c
Heesung jang <heesungj7@gmail.com>
2021-09-11 18:02:39 +0900

    [feat] 전역 themeProvider 생성 + 글로벌 스타일 적용
    overwritten 브라우저 기본 css 스타일
    
    -e.g) body margin

--- heesung_jang_test/src/index.js
+++ heesung_jang_test/src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import GlobalThemeProvider from "./styles/GlobalThemeProvider";
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <GlobalThemeProvider>
+            <App />
+        </GlobalThemeProvider>
     </React.StrictMode>,
     document.getElementById("root")
 );
