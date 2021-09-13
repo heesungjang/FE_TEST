e81ec3b9eb30a94945e4d98d477232ae6054ffac
Heesung jang <heesungj7@gmail.com>
2021-09-14 05:51:00 +0900

    [refactor] base URL 변경
    - env파일이 압축 파일에 들어가지 않아 기존 환경변수에 있는 baseURL -->  axios instance에서 하드 코딩

--- heesung_jang_test/src/shared/api.js
+++ heesung_jang_test/src/shared/api.js
@@ -2,15 +2,12 @@ import axios from "axios"; // http requests 라이브러리
 
 // Axios 인스턴스 설정
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: "http://testapi.hits.ai/",
 });
 
 //┏----------interceptor를 통한 header 설정----------┓
 instance.interceptors.request.use(async (config) => {
     config.headers["content-type"] = "application/json; charset=utf-8";
-    // config.headers["Access-Control-Allow-Origin"] = "*";
-    // config.headers["X-CSRFToken"] =
-    //     "JJazI8ddFOOcplrtEPFRlnlbovnoWzyehwkl1Osogg1FxOSkpSakkeGixZpQe5F1";
     config.headers["X-Requested-With"] = "XMLHttpRequest";
     config.headers["Accept"] = "*/*";
 
